6862038d4e76cfa40fe0e96f3869c19edd89e781
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-18 22:03:13 -0400

    add head function

---
+++ head.js
@@ -0,0 +1,16 @@
+// Assert function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Head function - return the first item in an array
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
