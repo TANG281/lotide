50bbd6752fa24da0708d93233370350801bf7667
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-19 17:51:08 -0400

    add without function

---
+++ without.js
@@ -0,0 +1,50 @@
+// Comparing 2 arrays
+// If their length are different -> return false
+// If they have the same length, iterate through each element of the first array and compare it to the same one on the second array
+// Iteration stops when result is fasle and continue if it is true
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length !== array2.length) {
+    result = false;
+    return result;
+  } else {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        result = false;
+        return result;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
+// Test code for arrays
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// WITHOUT function - return a NEW array with only elements from source array that are not included in itemsToRemove array
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.includes(source[i]) === false) {
+      result.push(source[i]);
+    }
+  }
+  return result;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(without(words, ["lighthouse"]));
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
