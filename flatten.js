1183d8b64dfaa2d04bcac61cbfa2f3115d974f0d
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-19 23:03:28 -0400

    add flatten function

---
+++ flatten.js
@@ -0,0 +1,49 @@
+// Comparing 2 arrays
+// If their length are different -> return false
+// If they have the same length, iterate through each element of the first array and compare it to the same one on the second array
+// Iteration stops when result is fasle and continue if it is true
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length !== array2.length) {
+    result = false;
+    return result;
+  } else {
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] !== array2[x]) {
+        result = false;
+        return result;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
+// Test code for arrays
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FLATTEN function
+const flatten = function(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      for (let j = 0; j < array[i].length; j++) {
+        result.push(array[i][j]);
+      }
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
+};
+
+// Check flatten function
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten(['nice', 'TGIF', [3, 4], 'Long weekend', [6]]), ['nice', 'TGIF', 3, 4, 'Long weekend', 6]);
