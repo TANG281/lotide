2f08da52a2c73e76f2280669eb9f4219c8316800
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-21 16:18:33 -0400

    update test codes

--- without.js
+++ without.js
@@ -40,11 +40,10 @@ const without = function(source, itemsToRemove) {
   return result;
 };
 
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+assertArraysEqual((without([1, 2, 3], [1])), [2, 3]);
+assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])), ["1", "2"]);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-console.log(without(words, ["lighthouse"]));
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
