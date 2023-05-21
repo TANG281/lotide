abcbaa7ff179f8ff81af154b53c1d553071f6127
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-21 13:31:17 -0400

    update comment

--- eqArrays.js
+++ eqArrays.js
@@ -10,7 +10,7 @@ const assertEqual = function(actual, expected) {
 // Comparing 2 arrays
 // If their length are different -> return false
 // If they have the same length, iterate through each element of the first array and compare it to the same one on the second array
-
+// Iteration stops when result is fasle and continue if it is true
 const eqArrays = function(array1, array2) {
   let result = true;
   if (array1.length !== array2.length) {
