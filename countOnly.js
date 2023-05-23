c34a5bf1b094137d5f6e4c3037715c024c585e63
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-23 00:57:16 -0400

    update code

--- countOnly.js
+++ countOnly.js
@@ -20,11 +20,9 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
+      if (!results[item]) {
         results[item] = 1;
-      }
+      } else results[item] += 1;
     }
   }
   return results;
