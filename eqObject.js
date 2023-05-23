be9f5bbc79239bba77b299b75299a1d1e9775827
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-23 00:31:01 -0400

    update comment

--- eqObject.js
+++ eqObject.js
@@ -28,6 +28,8 @@ const eqArrays = function(array1, array2) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+
+// Object comparison function
 const eqObjects = function(object1, object2) {
   const keyList1 = Object.keys(object1);
   const keyList2 = Object.keys(object2);
