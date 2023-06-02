3e3fd750a796dfa06e687905e74b60aae1d98883
Tuan Anh Nguyen <atngu312@gmail.com>
2023-06-02 17:16:31 -0400

    update code using filter method

--- countLetters.js
+++ countLetters.js
@@ -13,11 +13,15 @@ const assertEqual = function(actual, expected) {
 const countLetters = function(sentence) {
   let result = {};
   for (const letter of sentence) {
-    if (letter !== ' ') {
-      if (!result[letter]) {
-        result[letter] = 1;
-      } else result[letter] += 1;
+    if (letter === ' ') {
+    continue;
     }
+    
+    if (!result[letter]) {
+        result[letter] = 0;
+    }
+     
+    result[letter] += 1;
   }
   return result;
 };
