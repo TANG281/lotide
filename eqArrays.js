1ea20702067b0afcdf36b8229e741cd15b6c2add
Tuan Anh Nguyen <atngu312@gmail.com>
2023-06-07 21:54:20 -0400

    separate test code to another file and export fn

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,7 @@
-const { assertEqual } = require('./assertEqual');
-
 // Comparing 2 arrays
 // If their length are different -> return false
 // If they have the same length, iterate through each element of the first array and compare it to the same one on the second array
 // Iteration stops when result is fasle and continue if it is true
-
-// Array comparison function
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,12 +14,5 @@ const eqArrays = function(array1, array2) {
   
   return true;
 };
-
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays(["a", "2", "3"], ["A", "2", "3"]), false);
-// assertEqual(eqArrays([11, 222, 333], [11, 222, 333]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays(["1", "2", "3"], 1), false);
 
 module.exports = { eqArrays };
